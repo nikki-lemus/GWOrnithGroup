a5ba8c6b59e24b984ebe4fad0fa52ab8333faa60
AlexandraFiles <alexandrafiles@ku.edu>
2024-11-14 09:37:29 -0600

    Added coords to spreadsheet

--- SamplingMap.R
+++ SamplingMap.R
@@ -9,32 +9,35 @@ library(maps)
 library(mapdata)
 
 #set working directory
-setwd("~/KU/Fall2024/GitHubWorkshop/GW_ornith_group")
+setwd("~/KU/Fall2024/GitHubWorkshop/GWOrnithGroup")
 
 #read in sampling file
 #columns named Latitude, Longitude, and species are of interest
 data <- read.csv("./resurvey_data.csv")
 
+#ignore data with missing coordinates
+data <- data[!is.na(data$latitude),]
 #generate list of unique latitudes
-unq<-data[!duplicated(data$Latitude),]
+unq<-data[!duplicated(data$latitude),]
 #for each unique latitude, find how many samples are from there by adding a sample size column
-unq$sample.size<-as.vector(table(data$Latitude)[order(match(names(table(data$Latitude)),unq$Latitude))])
+unq$sample.size<-as.vector(table(data$latitude)[order(match(names(table(data$latitude)),unq$latitude))])
 
-#get map of Kansas from rgeoboundaries
+#get map of Kansas
 kansas <- subset(map_data("state"), region=="kansas")
-#set how big you want boundaries around points to be
-g=1
-#subset map outline based on sampling
-phil_bound <- st_crop(phil_bound, xmin = min(unq$Longitude)-g, xmax = max(unq$Longitude)+g,
-                      ymin = min(unq$Latitude)-g, ymax = max(unq$Latitude)+g)
+#get map of Kansas counties
+counties <- map_data("county")
+ks_counties <- subset(counties, region=="kansas")
 
 #plot figure
 ggplot(data=kansas, mapping=aes(x=long, y=lat, group=group)) +
+  geom_polygon(data=ks_counties, fill=NA, color="white") +
   geom_polygon(color="black", fill="gray") +
   theme(axis.text.x = element_blank(),
         axis.text.y = element_blank(),
         axis.ticks = element_blank(),
         rect = element_blank()) +
   #geom_point(data = unq, aes(x = Longitude, y = Latitude, color=Species, size=sample.size), show.legend=TRUE) +
-  #geom_point(data = unq, aes(x = Longitude, y = Latitude, size=sample.size), shape=21, colour="black")+
+  geom_point(data = unq, aes(x = longitude, y = latitude, size=sample.size), inherit.aes = FALSE, shape=21, colour="black") +
+  geom_polygon(data=ks_counties, fill=NA, color="white") +
+  geom_polygon(color="black", fill=NA) +
   labs(x = "", y = "", size="Count")
